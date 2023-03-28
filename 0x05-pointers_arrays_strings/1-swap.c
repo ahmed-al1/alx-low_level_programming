4ff5874d1a7df72e0a8d7f074d503aa6f1ab258c
ahmed <aelnegaralaa@gmail.com>
2023-03-27 17:14:05 -0700

    f

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * swap_int - is a fintions swaps the numbers
+ *
+ * @a: the first parameter
+ * @b: the socend parameter
+ *
+ **/
+
+void swap_int(int *a, int *b);
+
+{
+	int l;
+	*a = l;
+	*a = *b;
+	l = *b;
+
+}
