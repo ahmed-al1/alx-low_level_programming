59abea49bfa16920b53f66af7587dba85e2b767a
ahmed <aelnegaralaa@gmail.com>
2023-03-27 17:00:56 -0700

    f

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset _to_98 - it define an pinter and set it to a specifie
+ *
+ * @n: a pointer variable
+ *
+ * Return : nothing
+ **/
+
+void reset_to_98(int *n);
+
+{
+	*n = 98;
+}
