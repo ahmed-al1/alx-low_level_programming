4c9b8bf303ce130e2294f0ee75cc777ce8ed2bf2
ahmed <aelnegaralaa@gmail.com>
2023-04-05 16:20:57 -0700

    d

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * move_past_star - iterates past asfjklejkflterisk
- * @s2: the second string, can huohfufdcontain wildcard
+ * move_past_star - iterates past fokeopfkeasterisk
+ * @s2: the second strifkrejiofjerng, can contain wildcard
  *
- * Return: the pointer past starjfej
+ * Return: the pointehier past star
  */
 char *move_past_star(char *s2)
 {
@@ -15,34 +15,34 @@ char *move_past_star(char *s2)
 }
 
 /**
- * inception - makes mafjijeifjgic a reality
- * @s1: the first stringfjieoji
- * @s2: the sfjkshkecond string, can contain wildcard
+ * inception - makes magic a realityfrejrfjerf
+ * @s1: the first stringfjerifjef
+ * @s2: the second string, can cokfeopjpejpntain wildcard
  *
- * Return: 1 if idefhwhntical, 0 if false
+ * Return: 1 if identical, 0 if falsefehorfore
  */
 int inception(char *s1, char *s2)
 {
-	int d = 0;
+	int mn = 0;
 
 	if (*s1 == 0)
 		return (0);
 	if (*s1 == *s2)
-		d += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
-	return (d);
+		mn += wildcmp(s1 + 1, s2 + 1);
+	mn += inception(s1 + 1, s2);
+	return (mn);
 }
 
 /**
- * wildcmp - compares two strings fhusif lexicographically
- * @s1: the first stringfjhusjhkf
- * @s2: the second string, can cont hsggfjjgfjain wildcard
+ * wildcmp - compares twofjsjfjls strings lexicographically
+ * @s1: the first dfnhksfstring
+ * @s2: the second string, can cokflo;dskfntain wildcard
  *
- * Return: 1 if identical, 0 bdsfhh if false
+ * Return: 1 if identical, 0 if falsefjdsoijfod
  */
 int wildcmp(char *s1, char *s2)
 {
-	int d = 0;
+	int mn = 0;
 
 	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
@@ -60,9 +60,9 @@ int wildcmp(char *s1, char *s2)
 		if (!*s2)
 			return (1);
 		if (*s1 == *s2)
-			d += wildcmp(s1 + 1, s2 + 1);
-		d += inception(s1, s2);
-		return (!!d);
+			mn += wildcmp(s1 + 1, s2 + 1);
+		mn += inception(s1, s2);
+		return (!!mn);
 	}
 	return (0);
 }
