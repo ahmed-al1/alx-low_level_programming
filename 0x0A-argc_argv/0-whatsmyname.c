af501f89135e2eef1f2b7edf7676c53226fe0818
ahmed <aelnegaralaa@gmail.com>
2023-04-08 17:25:38 -0700

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed fldjgdby a new line
+ * @argc: intlfgko
+ * @argv: listfjgd
+ * Return: No thing
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
