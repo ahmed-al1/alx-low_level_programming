0d7c875720eb6ab3481d657f40a8243716680e47
ahmed <aelnegaralaa@gmail.com>
2023-04-08 17:36:13 -0700

    D

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,15 +13,15 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 	int n, l = 0, m = atoi(argv[1]);
-	int c[] = {25, 10, 5, 2, 1};
+	int cents[] = {25, 10, 5, 2, 1};
 
 	for (n = 0; n < 5; n++)
 	{
-		if (m >= c[i])
+		if (m >= cents[i])
 		{
-			l += m / c[n];
-			money = money % c[n];
-			if (m % c[n] == 0)
+			l += m / cents[n];
+			money = money % cents[n];
+			if (m % cents[n] == 0)
 			{
 				break;
 			}
